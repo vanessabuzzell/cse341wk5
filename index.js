923e4e65b341e837fbc7dfa23f9ded54aa904b2e
Vanessa Buzzell <vanessabuzzell@live.com>
2025-05-25 01:37:09 -0400

    save swagger changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app
- 
+.use(bodyParser.json())
 .use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Origin', '*');
     next();
