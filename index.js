f00cf2584e6ac31ed167f2c806704606f1f78522
Vanessa Buzzell <vanessabuzzell@live.com>
2025-06-14 23:19:30 -0400

    wk7 auth0 added

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: 'a long, randomly-generated string stored in env',
+  baseURL: 'http://localhost:3000',
+  clientID: 'n3PQl8SY24rkQD2l9RcdWNAyyeKHXIVo',
+  issuerBaseURL: 'https://dev-s1847bx0bi0gtjsv.us.auth0.com'
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
 
 app 
 .use(bodyParser.json())
@@ -11,6 +29,7 @@ app
     next();
   })
   .use('/', require('./routes'));
+
 
 mongodb.initDb((err) => {
   if (err) {
