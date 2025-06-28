eabfbab29952e93588a90e5192542ac4cdd90929
Vanessa Buzzell <vanessabuzzell@live.com>
2025-06-28 18:52:02 -0400

    wk 10 push before a pull

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const app = express();
 const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
-const { auth } = require('express-openid-connect');
+const { auth, requiresAuth } = require('express-openid-connect');
 
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
-  clientID: 'n3PQl8SY24rkQD2l9RcdWNAyyeKHXIVo',
-  issuerBaseURL: 'https://dev-s1847bx0bi0gtjsv.us.auth0.com'
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  
+  
 };
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
@@ -20,6 +22,10 @@ app.use(auth(config));
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+//gets profile of person logged in
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
 
 app 
